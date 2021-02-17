f20b003be1118e0d6386064e2dd22ba48a68fa0c
WayTechEng <70178222+WayTech-Eng@users.noreply.github.com>
2021-02-18 09:24:02 +1100

    Collision basics - cubes only

---
+++ Assets/ColliderCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderCheck : MonoBehaviour
+{
+    public GameObject sphere;
+    public GameObject excluder;
+    private Collider col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Trigger");
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        //Debug.Log("Stag");
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Colliding");
+        Destroy(excluder);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //col.
+        //var a = GetComponent<Collision>();
+        //Debug.Log(a);
+    }
+}
